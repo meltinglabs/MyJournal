bc9f229647e65e56aba860ad9d9c5ac5827591ce
lossendae <lossendae@gmail.com>
2012-04-28 01:30:50 +0200

    PhpDoc Added autop method for nicely formatted content even when not using any IDE Added get accessor for autop method Starting to copy / paste method from Shaun's Articles component Fixed treenode context menu for MyJournal

--- core/components/myjournal/model/myjournal/myjournal.class.php
+++ core/components/myjournal/model/myjournal/myjournal.class.php
@@ -43,7 +43,7 @@ class MyJournal extends modResource {
     }
     
     /**
-     * Get the controller path for our Articles type.
+     * Get the controller path for our MyJournal type.
      * 
      * {@inheritDoc}
      * @static
@@ -52,8 +52,151 @@ class MyJournal extends modResource {
      */
     public static function getControllerPath(xPDO &$modx) {
         return $modx->getOption('myjournal.core_path',null,$modx->getOption('core_path').'components/myjournal/').'controllers/container/';
+    }    
+    
+    public function get($k, $format = null, $formatTemplate= null){
+        switch($k){
+            case 'autop':
+                $content = $this->autop( $this->get('content') );
+                $value = $content;
+                break;
+            default:
+                $value = parent::get($k,$format,$formatTemplate);
+                break;
+        }
+        return $value;
     }
     
+    /**
+     * Replaces double line-breaks with paragraph elements.
+     *
+     * A group of regex replaces used to identify text formatted with newlines and
+     * replace double line-breaks with HTML paragraph tags. The remaining
+     * line-breaks after conversion become <<br />> tags, unless $br is set to '0'
+     * or 'false'.
+     *
+     * @param string $pee The text which has to be formatted.
+     * @param bool $br Optional. If set, this will convert all remaining line-breaks after paragraphing. Default true.
+     * @return string Text which has been converted into correct paragraph tags.
+     */
+    public function autop($pee, $br = true) {
+        $pre_tags = array();
+
+        if ( trim($pee) === '' )
+            return '';
+
+        $pee = $pee . "\n"; // just to make things a little easier, pad the end
+
+        if ( strpos($pee, '<pre') !== false ) {
+            $pee_parts = explode( '</pre>', $pee );
+            $last_pee = array_pop($pee_parts);
+            $pee = '';
+            $i = 0;
+
+            foreach ( $pee_parts as $pee_part ) {
+                $start = strpos($pee_part, '<pre');
+
+                // Malformed html?
+                if ( $start === false ) {
+                    $pee .= $pee_part;
+                    continue;
+                }
+
+                $name = "<pre></pre>";
+                $pre_tags[$name] = substr( $pee_part, $start ) . '</pre>';
+
+                $pee .= substr( $pee_part, 0, $start ) . $name;
+                $i++;
+            }
+
+            $pee .= $last_pee;
+        }
+
+        $pee = preg_replace('|<br />\s*<br />|', "\n\n", $pee);
+        // Space things out a little
+        $allblocks = '(?:table|thead|tfoot|caption|col|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|option|form|map|area|blockquote|address|math|style|p|h[1-6]|hr|fieldset|legend|section|article|aside|hgroup|header|footer|nav|figure|figcaption|details|menu|summary)';
+        $pee = preg_replace('!(<' . $allblocks . '[^>]*>)!', "\n$1", $pee);
+        $pee = preg_replace('!(</' . $allblocks . '>)!', "$1\n\n", $pee);
+        $pee = str_replace(array("\r\n", "\r"), "\n", $pee); // cross-platform newlines
+        if ( strpos($pee, '<object') !== false ) {
+            $pee = preg_replace('|\s*<param([^>]*)>\s*|', "<param$1>", $pee); // no pee inside object/embed
+            $pee = preg_replace('|\s*</embed>\s*|', '</embed>', $pee);
+        }
+        $pee = preg_replace("/\n\n+/", "\n\n", $pee); // take care of duplicates
+        // make paragraphs, including one at the end
+        $pees = preg_split('/\n\s*\n/', $pee, -1, true);
+        // $pees = preg_split('/\n\s*\n/', $pee, -1, PREG_SPLIT_NO_EMPTY);
+        $pee = '';
+        foreach ( $pees as $tinkle )
+            $pee .= '<p>' . trim($tinkle, "\n") . "</p>\n";
+        $pee = preg_replace('|<p>\s*</p>|', '', $pee); // under certain strange conditions it could create a P of entirely whitespace
+        $pee = preg_replace('!<p>([^<]+)</(div|address|form)>!', "<p>$1</p></$2>", $pee);
+        $pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee); // don't pee all over a tag
+        $pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee); // problem with nested lists
+        $pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
+        $pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
+        $pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee);
+        $pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+        if ( $br ) {
+            // $pee = preg_replace_callback('/<(script|style).*?<\/\\1>/s', '_autop_newline_preservation_helper', $pee);
+            $pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee); // optionally make line breaks
+            $pee = str_replace('<WPPreserveNewline />', "\n", $pee);
+        }
+        $pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee);
+        $pee = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee);
+        $pee = preg_replace( "|\n</p>$|", '</p>', $pee );
+
+        if ( !empty($pre_tags) )
+            $pee = str_replace(array_keys($pre_tags), array_values($pre_tags), $pee);
+
+        return $pee;
+    }
+
+    public function set($k, $v= null, $vType= '') {
+        $oldAlias = false;
+        if ($k == 'alias') {
+            $oldAlias = $this->get('alias');
+        }
+        $set = parent::set($k,$v,$vType);
+        if ($this->isDirty('alias') && !empty($oldAlias)) {
+            $this->oldAlias = $oldAlias;
+        }
+        return $set;
+    }
+    
+    public function save($cacheFlag = null) {
+        $isNew = $this->isNew();
+        $saved = parent::save($cacheFlag);
+        if ($saved && !$isNew && !empty($this->oldAlias)) {
+            $newAlias = $this->get('alias');
+            $saved = $this->updateChildrenURIs($newAlias,$this->oldAlias);
+        }
+        return $saved;
+    }
+    
+    /**
+    * Update all Articles URIs to reflect the new blog alias
+    *
+    * @param string $newAlias
+    * @param string $oldAlias
+    * @return bool
+    */
+    public function updateChildrenURIs($newAlias,$oldAlias) {
+        $useMultiByte = $this->getOption('use_multibyte',null,false) && function_exists('mb_strlen');
+        $encoding = $this->getOption('modx_charset',null,'UTF-8');
+        $oldAliasLength = ($useMultiByte ? mb_strlen($oldAlias,$encoding) : strlen($oldAlias)) + 1;
+        $uriField = $this->xpdo->escape('uri');
+
+        $sql = 'UPDATE '.$this->xpdo->getTableName('MyArticle').'
+        SET '.$uriField.' = CONCAT("'.$newAlias.'",SUBSTRING('.$uriField.','.$oldAliasLength.'))
+        WHERE
+        '.$this->xpdo->escape('parent').' = '.$this->get('id').'
+        AND SUBSTRING('.$uriField.',1,'.$oldAliasLength.') = "'.$oldAlias.'/"';
+        $this->xpdo->log(xPDO::LOG_LEVEL_DEBUG,$sql);
+        $this->xpdo->exec($sql);
+        return true;
+    }
+
     /**
      * Provide the name of this CRT.
      * {@inheritDoc}
@@ -64,72 +207,65 @@ class MyJournal extends modResource {
     }
     
     /**
-     * Provide the custom context menu for Articles.
+     * Provide the custom context menu for MyJournal.
      * {@inheritDoc}
      * @return array
      */
     public function getContextMenuText() {
         return array(
             'text_create' => 'Journal',
-            'text_create_here' => 'Create a Journal Here',
+            'text_create_here' => 'Create a new Journal Here',
         );
-    }
-    
+    }    
     
     /**
      * @param array $node
      * @return array
      */
     public function prepareTreeNode(array $node = array()) {
-        // $this->xpdo->lexicon->load('articles:default');
+        // $this->xpdo->lexicon->load('myjournal:default');
         $menu = array();
         $menu[] = array(
-            'text' => '<b>'.$this->get('pagetitle').'</b>',
-            'handler' => 'Ext.emptyFn',
+            'text' => 'Edit Journal',
+            'handler' => 'this.editResource',
         );
-        // $menu[] = '-';
-        // $menu[] = array(
-            // 'text' => $this->xpdo->lexicon('articles.articles_manage'),
-            // 'handler' => 'this.editResource',
-        // );
-        // $menu[] = array(
-            // 'text' => $this->xpdo->lexicon('articles.articles_write_new'),
-            // 'handler' => 'function(itm,e) { itm.classKey = "Article"; this.createResourceHere(itm,e); }',
-        // );
-        // $menu[] = '-';
-        // if ($this->get('published')) {
-            // $menu[] = array(
-                // 'text' => $this->xpdo->lexicon('articles.container_unpublish'),
-                // 'handler' => 'this.unpublishDocument',
-            // );
-        // } else {
-            // $menu[] = array(
-                // 'text' => $this->xpdo->lexicon('articles.container_publish'),
-                // 'handler' => 'this.publishDocument',
-            // );
-        // }
-        // if ($this->get('deleted')) {
-            // $menu[] = array(
-                // 'text' => $this->xpdo->lexicon('articles.container_undelete'),
-                // 'handler' => 'this.undeleteDocument',
-            // );
-        // } else {
-            // $menu[] = array(
-                // 'text' => $this->xpdo->lexicon('articles.container_delete'),
-                // 'handler' => 'this.deleteDocument',
-            // );
-        // }
-        // $menu[] = '-';
-        // $menu[] = array(
-            // 'text' => $this->xpdo->lexicon('articles.articles_view'),
-            // 'handler' => 'this.preview',
-        // );
-
+        $menu[] = array(
+            'text' => 'View Journal (Front end)',
+            'handler' => 'this.preview',
+        );
+        $menu[] = '-';
+        if ($this->get('published')) {
+            $menu[] = array(
+                'text' => 'Publish Journal',
+                'handler' => 'this.unpublishDocument',
+            );
+        } else {
+            $menu[] = array(
+                'text' => 'Unpublish Journal',
+                'handler' => 'this.publishDocument',
+            );
+        }
+        if ($this->get('deleted')) {
+            $menu[] = array(
+                'text' => 'Undelete Journal',
+                'handler' => 'this.undeleteDocument',
+            );
+        } else {
+            $menu[] = array(
+                'text' => 'Delete Journal',
+                'handler' => 'this.deleteDocument',
+            );
+        }
+        $menu[] = '-';
+        $menu[] = array(
+            'text' => 'Create a new Article (MyArticle)',
+            'handler' => 'function(itm,e) { itm.classKey = "MyArticle"; this.createResourceHere(itm,e); }',
+        );
         $node['menu'] = array('items' => $menu);
         return $node;
     }
     
-     /**
+    /**
      * Override modResource::process to set some custom placeholders for the Resource when rendering it in the front-end.
      * {@inheritDoc}
      * @return string
@@ -140,6 +276,12 @@ class MyJournal extends modResource {
         return $this->_content;
     }
     
+    /**
+     * Get the getPage and getArchives call to display listings of posts on the container.
+     *
+     * @param string $placeholderPrefix
+     * @return string
+     */
     public function setPostListingCall(){
         $where = array('class_key' => 'MyArticle');
         //@TODO : This will be the settings for myjournal later
@@ -169,7 +311,7 @@ class MyJournal extends modResource {
             'pageNavVar' => $this->xpdo->getOption('page_nav_var', $settings, 'page.nav'),
             'totalVar' => $this->xpdo->getOption('page_total_var', $settings, 'total'),
             'offset' => $this->xpdo->getOption('page_offset', $settings, 0),
-            'tpl' => $this->xpdo->getOption('articles_tpl', $settings, 'myjournal/article.tpl'),
+            'tpl' => $this->xpdo->getOption('articles_tpl', $settings, 'myjournal/articles.tpl'),
         );
         
         if($this->xpdo->getOption('set_page_nav_placeholder', $settings, true)){
@@ -180,12 +322,19 @@ class MyJournal extends modResource {
                 </ul>
                 </div>
             `]]');
-        }
-        
+        }        
         $this->setTagCall('getPage', $articlesPlaceholder, false, $options);
     }
     
-    public function setTagCall($name, $placeholder, $cached = false, $options = array()){
+    /**
+     * Set modx tags for the specified placeholder
+     *
+     * @param string $name The snippet name
+     * @param string $targetPlaceholder The placeholder to set
+     * @param boolean $cached Whether the snippet call should be cached or not
+     * @param array $options An array of options to override default properties
+     */
+    public function setTagCall($name, $targetPlaceholder, $cached = false, $options = array()){
         $tags = ($cached) ? "[[": "[[!";
         $tags .= $name."?";        
         foreach($options as $key => $value){
@@ -196,16 +345,41 @@ class MyJournal extends modResource {
         if($this->xpdo->getOption('myjournal.debug_tag_call', null, false)){
             $debugTags = str_replace('[[','&#91;&#91;', $tags);
             $debugTags = str_replace(']]','&#93;&#93;', $debugTags);
-            $this->xpdo->setPlaceholder('debug_tag_'.$placeholder, '<pre>'. $debugTags .'</pre>');
+            $this->xpdo->setPlaceholder('debug_tag_'.$targetPlaceholder, '<pre>'. $debugTags .'</pre>');
         }
-        $this->xpdo->setPlaceholder($placeholder, $tags);
+        $this->xpdo->setPlaceholder($targetPlaceholder, $tags);
+        /* Might be useful - Using a 3rd party nippet */
+        $this->xpdo->setPlaceholder('layout_prefix', 'articles-list-');
+        $this->xpdo->setPlaceholder('layout_suffix', '-articles-list');
     }
     
+    /**
+     * Get an array of settings for the resource.
+     * @return array
+     */
+    public function getSettings() {
+        $settings = $this->getProperties('articles');
+        if (!empty($settings)) {
+            $settings = is_array($settings) ? $settings : $this->xpdo->fromJSON($settings);
+        }
+        return !empty($settings) ? $settings : array();
+    }
+    
+    /**
+     * @param array $options
+     * @return string
+     */
     public function getContent(array $options = array()) {
         $content = parent::getContent($options);
         return $content;
     }
 }
+
+/**
+ * Overrides the modResourceCreateProcessor to provide custom processor functionality for the MyJournal type
+ *
+ * @package myjournal
+ */
 class MyJournalCreateProcessor extends modResourceCreateProcessor {
     /** @var MyJournal $object */
     public $object;
@@ -217,7 +391,7 @@ class MyJournalCreateProcessor extends modResourceCreateProcessor {
      * @return boolean
      */
     public function beforeSave() {
-        $this->object->set('content','[[!MyJournal]]');
+        $this->object->set('content','[[+articles]]');
         $this->object->set('class_key','MyJournal');
         $this->object->set('richtext',false);
         /* We need to set a compatible remark theme as soon as possible */
@@ -240,8 +414,7 @@ class MyJournalCreateProcessor extends modResourceCreateProcessor {
     
     /**
      * Save the Resource Groups on the object
-     * 
-     * @return void
+     * Override the default implementation to use results from checkboxes instead of a grid store
      */
     public function saveResourceGroups() {
         $attributted = array();
@@ -293,12 +466,19 @@ class MyJournalCreateProcessor extends modResourceCreateProcessor {
         }
     }
 }
+
+/**
+ * Overrides the modResourceCreateProcessor to provide custom processor functionality for the MyJournal type
+ *
+ * @package myjournal
+ */
 class MyJournalUpdateProcessor extends modResourceUpdateProcessor {
     /** @var MyJournal $object */
     public $object;
     
     /**
      * If specified, set the Resource Groups attached to the Resource
+     * Override the default implementation to use results from checkboxes instead of a grid store
      * @return mixed
      */
     public function setResourceGroups() {
